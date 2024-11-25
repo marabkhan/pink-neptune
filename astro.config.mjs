f9372c788b2498bd33b0f79ff2f45c77de45dde0
marabk03 <143238766+marabk03@users.noreply.github.com>
2024-11-25 15:08:04 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
+import vercelServerless from '@astrojs/vercel/serverless';
 
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true,
-    imageCDN: false,
-  })
+  adapter: vercelServerless(),
 });
