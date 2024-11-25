56a66f9b717c0d8a43f549f0cdf150a34e3115cd
marabk03 <143238766+marabk03@users.noreply.github.com>
2024-11-25 15:14:06 -0500

    trying static vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercelStatic from '@astrojs/vercel/static';
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
+  output: 'static',
+  adapter: vercelStatic(),
 });
