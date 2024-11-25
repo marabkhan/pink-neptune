40aa1abf2bc673160814059fc7b2602682e14e90
marabk03 <143238766+marabk03@users.noreply.github.com>
2024-11-25 15:09:54 -0500

    vercel fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: vercelServerless(),
+  output: 'server',
+  adapter: vercel(),
 });
