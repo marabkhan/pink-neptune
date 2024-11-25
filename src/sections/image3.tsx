b663006b305685762e22aae8959fcfa2a3bb79a7
marabk03 <143238766+marabk03@users.noreply.github.com>
2024-11-25 16:03:55 -0500

    removed random img

--- src/sections/image3.tsx
+++ src/sections/image3.tsx
@@ -5,7 +5,6 @@ import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
 import image4 from '../assets/image4.jpg';
 import image5 from '../assets/image5.jpg';
-import image6 from '../assets/image6.jpg';
 
 
 type ImageMetadata = {
@@ -15,7 +14,7 @@ type ImageMetadata = {
   format?: string; 
 };
 
-const images: ImageMetadata[] = [image1, image2, image3, image4, image5, image6, ];
+const images: ImageMetadata[] = [image1, image2, image3, image4, image5, ];
 
 const ImageGallery = () => {
   return (
